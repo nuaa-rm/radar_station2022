07356bcc9680c7888021bf0f998f36c72cbf0a6b
dovejh <1358446393@qq.com>
2022-06-08 00:19:26 +0800

    [feature]添加了红蓝方参数，添加了接收消息和车间通信。

--- serial_port/src/serial_port_node.cpp
+++ serial_port/src/serial_port_node.cpp
@@ -24,7 +24,7 @@ struct map_msg_data//小地图消息数据
     float target_position_x;
     float target_position_y;
 } __attribute__((packed));
-struct map_msg//小地图消息
+struct map_msg//小地图消息10HZ
 {
     frame_header head;
     uint16_t cmd_id = 0x0305;
@@ -36,13 +36,28 @@ struct interactive_with_robots_msgs_data
     uint16_t cmd_id;
     uint16_t sender_id;
     uint16_t receiver_id;
-
+    uint8_t number = 0;
 } __attribute__((packed));
-struct interactive_with_robots_msgs
+struct interactive_with_robots_msgs//目前是专门给工程写的
 {
     frame_header head;
     uint16_t cmd_id = 0x0301;
     interactive_with_robots_msgs_data data;
+    uint16_t crc;
+} __attribute__((packed));//最大10HZ
+struct game_status_msgs_data
+{
+    uint8_t game_type = 1;
+    uint16_t game_progress = 0;
+    uint16_t stage_remain_time = 0;
+    uint64_t SyncTimeStamp = 0;
+
+} __attribute__((packed));
+struct game_status_msgs //1HZ
+{
+    frame_header head;
+    uint16_t cmd_id = 0x0000;
+    game_status_msgs_data data;
     uint16_t crc;
 } __attribute__((packed));
 class serial_port
@@ -51,6 +66,9 @@ public:
     serial::Serial ser;
     map_msg mapMsg;
     interactive_with_robots_msgs interactiveWithRobotsMsgs;
+    game_status_msgs gameStatusMsgs;
+    uint8_t receiveData[200];
+    bool is_enemy_red = true;
     //vector<point>points;
     int serial_port_init()
     {
@@ -75,34 +93,53 @@ public:
         mapMsg.data.target_position_y = y;
         mapMsg.data.target_robot_id = id;
         mapMsg.crc = get_CRC16_check_sum((uint8_t*)&mapMsg, (sizeof(mapMsg) - sizeof(mapMsg.crc)), 0xffff);
-        /*char msg[sizeof (mapMsg)];
-        uint8_t* p = mapMsg;
-        for(int i = 0; i < sizeof(mapMsg); i++)
-        {
-            msg[i] = *p;
-            p++;
-        }*/
-        //ser.write(msg);
+
         ser.write((uint8_t* )&mapMsg, sizeof(map_msg));
         cout << "Send one map msg target_id = " << mapMsg.data.target_robot_id << " x = " << mapMsg.data.target_position_x << " y = " << mapMsg.data.target_position_y << endl;
         return true;
     }
-    bool sendInteractiveMsgs()
+    bool sendInteractiveMsgs(uint8_t number)//只适用于工程
     {
         interactiveWithRobotsMsgs.head.SOF = 0xA5;
-
-        /*string msg;
-        for(int i = 0; i < sizeof(interactiveWithRobotsMsgs); i++)
+        interactiveWithRobotsMsgs.head.data_length = 7;
+        interactiveWithRobotsMsgs.head.seq = 1;
+        interactiveWithRobotsMsgs.head.crc = get_CRC8_check_sum((uint8_t*)&interactiveWithRobotsMsgs, (sizeof(interactiveWithRobotsMsgs.head) - sizeof(interactiveWithRobotsMsgs.head.crc)), 0xff);
+        interactiveWithRobotsMsgs.cmd_id = 0x0301;
+        interactiveWithRobotsMsgs.data.cmd_id = 0x0200;
+        if(is_enemy_red)
         {
-            msg += ((char*)&interactiveWithRobotsMsgs + i);
-        }*/
-        //ser.write((uint8_t* )mapMsg, sizeof(mapMsg));
+            interactiveWithRobotsMsgs.data.sender_id = 109;
+            interactiveWithRobotsMsgs.data.receiver_id = 102;
+        }
+        else
+        {
+            interactiveWithRobotsMsgs.data.sender_id = 9;
+            interactiveWithRobotsMsgs.data.receiver_id = 2;
+        }
+
+        interactiveWithRobotsMsgs.data.number = number;
+        interactiveWithRobotsMsgs.crc = get_CRC16_check_sum((uint8_t*)&interactiveWithRobotsMsgs, (sizeof(interactiveWithRobotsMsgs) - sizeof(interactiveWithRobotsMsgs.crc)), 0xffff);
+
+        ser.write((uint8_t* )&interactiveWithRobotsMsgs, sizeof(interactiveWithRobotsMsgs));
+
         cout << "Send one interactive msg " << endl;
         return true;
     }
     bool receiveMsgs()
     {
-        return true;
+        if(ser.available())
+        {
+            ser.read(receiveData, ser.available());
+            gameStatusMsgs = *(game_status_msgs*)receiveData;
+            if((gameStatusMsgs.head.crc == get_CRC8_check_sum((uint8_t*)&gameStatusMsgs, (sizeof(gameStatusMsgs.head) - sizeof(gameStatusMsgs.head.crc)), 0xff)) && (gameStatusMsgs.crc == get_CRC16_check_sum((uint8_t*)&gameStatusMsgs, (sizeof(gameStatusMsgs) - sizeof(gameStatusMsgs.crc)), 0xffff)))
+            {
+
+                //这里还没写完，需要新的消息。
+
+                return true;
+            }
+        }
+        return false;
     }
 
 };
@@ -110,7 +147,6 @@ public:
 void imgCallback(const radar_msgs::small_mapConstPtr& msg)
 {
     msg->points;
-
 }
 
 int main (int argc, char** argv)
@@ -130,7 +166,7 @@ int main (int argc, char** argv)
     {
         ROS_INFO_STREAM("Serial Port initialized! ");
     }
-
+    ros::param::get("is_enemy_red", sp.is_enemy_red);
 
     ros::Rate loop(10);
     ROS_INFO_STREAM("Looping! ");
@@ -138,7 +174,7 @@ int main (int argc, char** argv)
     while(ros::ok())
     {
         sp.sendMapMsgs(6, (float)x, (float)y);
-
+        sp.receiveMsgs();
         x++;
         y++;
         if(x >= 28)
