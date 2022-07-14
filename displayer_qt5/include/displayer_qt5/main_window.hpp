619004e5c50c5dd26ef2a690b47aa06c1bbfd34d
dovejh <1358446393@qq.com>
2022-07-14 19:12:59 +0800

    [factor]添加基于qt5的displayer包

---
+++ displayer_qt5/include/displayer_qt5/main_window.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file /include/displayer_qt5/main_window.hpp
+ *
+ * @brief Qt based gui for displayer_qt5.
+ *
+ * @date November 2010
+ **/
+#ifndef displayer_qt5_MAIN_WINDOW_H
+#define displayer_qt5_MAIN_WINDOW_H
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui/QMainWindow>
+#include "ui_main_window.h"
+#include "qnode.hpp"
+
+/*****************************************************************************
+** Namespace
+*****************************************************************************/
+
+namespace displayer_qt5 {
+
+/*****************************************************************************
+** Interface [MainWindow]
+*****************************************************************************/
+/**
+ * @brief Qt central, all operations relating to the view part here.
+ */
+class MainWindow : public QMainWindow {
+Q_OBJECT
+
+public:
+	MainWindow(int argc, char** argv, QWidget *parent = 0);
+	~MainWindow();
+
+	void ReadSettings(); // Load up qt program settings at startup
+	void WriteSettings(); // Save qt program settings when closing
+
+	void closeEvent(QCloseEvent *event); // Overloaded function
+	void showNoMasterMessage();
+
+public Q_SLOTS:
+	/******************************************
+	** Auto-connections (connectSlotsByName())
+	*******************************************/
+	void on_actionAbout_triggered();
+	void on_button_connect_clicked(bool check );
+	void on_checkbox_use_environment_stateChanged(int state);
+
+    /******************************************
+    ** Manual connections
+    *******************************************/
+    void updateLoggingView(); // no idea why this can't connect automatically
+
+private:
+	Ui::MainWindowDesign ui;
+	QNode qnode;
+};
+
+}  // namespace displayer_qt5
+
+#endif // displayer_qt5_MAIN_WINDOW_H
---
+++ displayer_qt5/include/displayer_qt5/qnode.hpp
@@ -0,0 +1,75 @@
+/**
+ * @file /include/displayer_qt5/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef displayer_qt5_QNODE_HPP_
+#define displayer_qt5_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+// To workaround boost/qt4 problems that won't be bugfixed. Refer to
+//    https://bugreports.qt.io/browse/QTBUG-22829
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+#endif
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace displayer_qt5 {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+Q_SIGNALS:
+	void loggingUpdated();
+    void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+	ros::Publisher chatter_publisher;
+    QStringListModel logging_model;
+};
+
+}  // namespace displayer_qt5
+
+#endif /* displayer_qt5_QNODE_HPP_ */
