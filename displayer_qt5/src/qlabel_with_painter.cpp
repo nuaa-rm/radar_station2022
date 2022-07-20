c44287f0916bdf5e6c9366fc9ef54379dae785d8
dovejh <1358446393@qq.com>
2022-07-20 20:49:48 +0800

    [factor]添加了小地图画图程序，有bug

---
+++ displayer_qt5/src/qlabel_with_painter.cpp
@@ -0,0 +1,6 @@
+#include "qlabel_with_painter.h"
+
+QLabel_with_painter::QLabel_with_painter()
+{
+
+}
---
+++ displayer_qt5/src/qlabel_with_painter.h
@@ -0,0 +1,13 @@
+#ifndef QLABEL_WITH_PAINTER_H
+#define QLABEL_WITH_PAINTER_H
+
+#include <QMainWindow>
+#include <QObject>
+
+class QLabel_with_painter
+{
+public:
+    QLabel_with_painter();
+};
+
+#endif // QLABEL_WITH_PAINTER_H
