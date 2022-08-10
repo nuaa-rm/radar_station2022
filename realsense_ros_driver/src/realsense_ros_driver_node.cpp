84704b69c80fef4efba0ca806f1a6b8063b07bf0
dovejh <1358446393@qq.com>
2022-08-10 10:07:52 +0800

    [fix]解决了realsense卡顿的问题

--- realsense_ros_driver/src/realsense_ros_driver_node.cpp
+++ realsense_ros_driver/src/realsense_ros_driver_node.cpp
@@ -14,28 +14,28 @@ using namespace std;
 using namespace cv;
 
 // 相机内参
-const double camera_factor = 1000;
-const double camera_cx = 311.704;
-const double camera_cy = 245.82;
-const double camera_fx = 474.055;
-const double camera_fy = 474.055;
+//const double camera_factor = 1000;
+//const double camera_cx = 311.704;
+//const double camera_cy = 245.82;
+//const double camera_fx = 474.055;
+//const double camera_fy = 474.055;
 
 ros::Publisher img_pub;
 
 
-void get_img(ros::NodeHandle nh)
-{
-    rs2::pipeline pipe;     //Contruct a pipeline which abstracts the device
-    rs2::config cfg;    //Create a configuration for configuring the pipeline with a non default profile
+//void get_img(ros::NodeHandle nh)
+//{
+//    rs2::pipeline pipe;     //Contruct a pipeline which abstracts the device
+//    rs2::config cfg;    //Create a configuration for configuring the pipeline with a non default profile
 //
 //    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
 //
 //    rs2::pipeline_profile selection = pipe.start(cfg);
 //    rs2_stream align_to = RS2_STREAM_COLOR;
 //    rs2::align align(align_to);
-
-    while(ros::ok())
-    {
+//
+//    while(ros::ok())
+//    {
 //        rs2::frameset frames;
 //        frames = pipe.wait_for_frames();
 //
@@ -50,8 +50,8 @@ void get_img(ros::NodeHandle nh)
 //        sensor_msgs::ImagePtr color_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", color).toImageMsg();
 //        color_msg->header.stamp = ros::Time::now();
 //        img_pub.publish(color_msg);
-    }
-}
+//    }
+//}
 
 int main (int argc, char** argv)
 {
@@ -59,31 +59,25 @@ int main (int argc, char** argv)
     ros::init(argc, argv, "realsense_ros_driver_node");
     //声明节点句柄
     ros::NodeHandle nh;
-    img_pub=nh.advertise<sensor_msgs::Image>("/camera/color/image_raw",10);
+    img_pub=nh.advertise<sensor_msgs::Image>("/camera/color/image_raw",1);
     ros::Rate loop_rate(30);
-
     rs2::pipeline pipe;     //Contruct a pipeline which abstracts the device
-    rs2::config cfg;    //Create a configuration for configuring the pipeline with a non default profile
-
-    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
-
-    rs2::pipeline_profile selection = pipe.start(cfg);
+//    rs2::config cfg;    //Create a configuration for configuring the pipeline with a non default profile
+//    cfg.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_BGR8, 30);
+//    rs2::pipeline_profile selection = pipe.start(cfg);
+    rs2::pipeline_profile selection = pipe.start();
     rs2_stream align_to = RS2_STREAM_COLOR;
     rs2::align align(align_to);
-
     while(ros::ok())
     {
         rs2::frameset frames;
         frames = pipe.wait_for_frames();
-
         auto processed = align.process(frames);
-
         //Get each frame
         auto color_frame = processed.get_color_frame();
-
         //create cv::Mat from rs2::frame
-        Mat color(Size(640, 480), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
-
+        Mat color(Size(1280, 720), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
+        cvtColor(color, color, CV_RGB2BGR);
         sensor_msgs::ImagePtr color_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", color).toImageMsg();
         color_msg->header.stamp = ros::Time::now();
         img_pub.publish(color_msg);
